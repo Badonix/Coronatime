9e6779127164d1e270aa93cf8aa09bebcd93e788
nick <nikoloz.danelia@redberry.ge>
2023-05-02 19:56:43 +0400

    feat: checkmarks for correct inputs

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,3 +1,8 @@
-<input
-    class='w-full px-5 mt-2 py-4 rounded-lg text-zinc-500 text-base border {{ $errors->has($attributes['name']) ? 'border-red-700' : ($attributes['value'] != null ? 'border-green-600' : 'border-neutral-200') }}'
-    {{ $attributes }} />
+<div class='relative w-full mt-2'>
+    <input
+        class='w-full px-5 py-4 rounded-lg text-zinc-500 text-base border {{ $errors->has($attributes['name']) ? 'border-red-700' : ($attributes['value'] != null ? 'border-green-600' : 'border-neutral-200') }}'
+        {{ $attributes }} />
+    @if ($attributes['value'] != null && !$errors->has($attributes['name']))
+        <img class='absolute w-6 right-4 top-1/2 transform -translate-y-1/2' src="{{ asset('/assets/tick.png') }}" />
+    @endif
+</div>
