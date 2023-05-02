71f1829d3e86e0b30fbb20b2d4a6116927ba39be
nick <nikoloz.danelia@redberry.ge>
2023-05-02 20:07:03 +0400

    style: favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coronatime</title>
+    <link rel='icon' href='{{ asset('/assets/favicon.png') }}' />
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
